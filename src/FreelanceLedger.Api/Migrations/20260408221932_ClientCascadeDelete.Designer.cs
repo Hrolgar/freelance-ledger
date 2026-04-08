e1af75a9c1edf1db3b3f6198275f0ebbac804c37
Helgi Skaftason <helgiskafta@gmail.com>
2026-04-09 00:19:39 +0200

    Cascade delete: deleting a client deletes its projects

--- src/FreelanceLedger.Api/Migrations/20260408221932_ClientCascadeDelete.Designer.cs
+++ src/FreelanceLedger.Api/Migrations/20260408221932_ClientCascadeDelete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FreelanceLedger.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FreelanceLedger.Api.Migrations
 {
     [DbContext(typeof(LedgerDbContext))]
-    partial class LedgerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260408221932_ClientCascadeDelete")]
+    partial class ClientCascadeDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
---
+++ src/FreelanceLedger.Api/Migrations/20260408221932_ClientCascadeDelete.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreelanceLedger.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Clients_ClientId",
+                table: "Projects");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Clients_ClientId",
+                table: "Projects",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Clients_ClientId",
+                table: "Projects");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Clients_ClientId",
+                table: "Projects",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
