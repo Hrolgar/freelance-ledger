fc7269941719898191e6bd80751563d23072cac2
Helgi Skaftason <helgiskafta@gmail.com>
2026-04-08 20:59:56 +0200

    Remove boilerplate API scaffold and wire ledger config

--- src/FreelanceLedger.Api/Program.cs
+++ src/FreelanceLedger.Api/Program.cs
@@ -1,23 +1,44 @@
+using FreelanceLedger.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<LedgerDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=ledger.db"));
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    var db = scope.ServiceProvider.GetRequiredService<LedgerDbContext>();
+    db.Database.Migrate();
 }
 
-app.UseHttpsRedirection();
+app.MapOpenApi();
+app.MapScalarApiReference();
 
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
